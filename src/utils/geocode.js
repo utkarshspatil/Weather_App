4010638fb802e73b7b2571e712ee0a0106b223eb
utkarshspatil <37769388+utkarshspatil@users.noreply.github.com>
2020-04-26 16:35:46 +0530

    minor

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const geocode = (address, callback) => {
 	const encodedAddress = encodeURIComponent(address);
 	const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodedAddress +'.json?access_token=pk.eyJ1IjoidXRrcGF0aWwiLCJhIjoiY2s5NTljdDBrMDMwNTNlbXBycnMzbXVsbiJ9.u35QIT7qgTIv2M980dg1IA';
-		axios.get(url)
+		axios
+		.get(url)
 		.then(({ data } = {}) => {
 			const location = data.features[0].place_name;
 				callback(undefined, {
